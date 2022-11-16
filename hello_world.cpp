9e4226ff0b8c2c92a1b76985e027b4c021765b40
Ramil Yagafarov <r.yagafarov1@gmail.com>
2022-11-16 18:28:07 +0300

    First commit

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world" << std::endl;
+    std::cout << "World Hello" << std::endl;
+    std::cout << "World Hello 3" << std::endl;
+    return 0;
+}
